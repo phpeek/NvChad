ddd8b85ffe49e16fc020725e0f02627c671dd9cb
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2023-10-12 22:37:47 +0200

    feat: install treesitter parsers

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,7 +2,39 @@ local plugins = {
   { "ThePrimeagen/vim-be-good" , cmd = "VimBeGood" },
 
   -- git integration
-  { "tpope/vim-fugitive", lazy = false }
+  { "tpope/vim-fugitive", lazy = false },
+
+  { "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "lua",
+        "vim",
+
+        "vimdoc",
+
+        -- go
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+
+        -- docker
+        "dockerfile",
+
+        -- makefiles
+        "make",
+
+        -- web dev
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json"
+      }
+    }
+  }
 }
 
 return plugins
